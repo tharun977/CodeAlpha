235bd89cdad79a35bf0e698f2504e6f6d1ba4095
Tharun Raman <116884536+tharun977@users.noreply.github.com>
2024-09-02 17:14:21 +0530

    Calculator

--- task2/script.js
+++ task2/script.js
@@ -1,137 +1,137 @@
-let firstOperand = ''
-let secondOperand = ''
-let currentOperation = null
-let shouldResetScreen = false
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operatorButtons = document.querySelectorAll('[data-operator]')
-const equalsButton = document.getElementById('equalsBtn')
-const clearButton = document.getElementById('clearBtn')
-const deleteButton = document.getElementById('deleteBtn')
-const pointButton = document.getElementById('pointBtn')
-const lastOperationScreen = document.getElementById('lastOperationScreen')
-const currentOperationScreen = document.getElementById('currentOperationScreen')
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.getElementById('equalsBtn');
+const clearButton = document.getElementById('clearBtn');
+const deleteButton = document.getElementById('deleteBtn');
+const pointButton = document.getElementById('pointBtn');
+const lastOperationScreen = document.getElementById('lastOperationScreen');
+const currentOperationScreen = document.getElementById('currentOperationScreen');
 
-window.addEventListener('keydown', handleKeyboardInput)
-equalsButton.addEventListener('click', evaluate)
-clearButton.addEventListener('click', clear)
-deleteButton.addEventListener('click', deleteNumber)
-pointButton.addEventListener('click', appendPoint)
+window.addEventListener('keydown', handleKeyboardInput);
+equalsButton.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', deleteNumber);
+pointButton.addEventListener('click', appendPoint);
 
 numberButtons.forEach((button) =>
   button.addEventListener('click', () => appendNumber(button.textContent))
-)
+);
 
 operatorButtons.forEach((button) =>
   button.addEventListener('click', () => setOperation(button.textContent))
-)
+);
 
 function appendNumber(number) {
   if (currentOperationScreen.textContent === '0' || shouldResetScreen)
-    resetScreen()
-  currentOperationScreen.textContent += number
+    resetScreen();
+  currentOperationScreen.textContent += number;
 }
 
 function resetScreen() {
-  currentOperationScreen.textContent = ''
-  shouldResetScreen = false
+  currentOperationScreen.textContent = '';
+  shouldResetScreen = false;
 }
 
 function clear() {
-  currentOperationScreen.textContent = '0'
-  lastOperationScreen.textContent = ''
-  firstOperand = ''
-  secondOperand = ''
-  currentOperation = null
+  currentOperationScreen.textContent = '0';
+  lastOperationScreen.textContent = '';
+  firstOperand = '';
+  secondOperand = '';
+  currentOperation = null;
 }
 
 function appendPoint() {
-  if (shouldResetScreen) resetScreen()
+  if (shouldResetScreen) resetScreen();
   if (currentOperationScreen.textContent === '')
-    currentOperationScreen.textContent = '0'
-  if (currentOperationScreen.textContent.includes('.')) return
-  currentOperationScreen.textContent += '.'
+    currentOperationScreen.textContent = '0';
+  if (currentOperationScreen.textContent.includes('.')) return;
+  currentOperationScreen.textContent += '.';
 }
 
 function deleteNumber() {
   currentOperationScreen.textContent = currentOperationScreen.textContent
     .toString()
-    .slice(0, -1)
+    .slice(0, -1);
 }
 
 function setOperation(operator) {
-  if (currentOperation !== null) evaluate()
-  firstOperand = currentOperationScreen.textContent
-  currentOperation = operator
-  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
-  shouldResetScreen = true
+  if (currentOperation !== null) evaluate();
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+  shouldResetScreen = true;
 }
 
 function evaluate() {
-  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === null || shouldResetScreen) return;
   if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
-    alert("You can't divide by 0!")
-    return
+    alert("You can't divide by 0!");
+    return;
   }
-  secondOperand = currentOperationScreen.textContent
+  secondOperand = currentOperationScreen.textContent;
   currentOperationScreen.textContent = roundResult(
     operate(currentOperation, firstOperand, secondOperand)
-  )
-  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
-  currentOperation = null
+  );
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+  currentOperation = null;
 }
 
 function roundResult(number) {
-  return Math.round(number * 1000) / 1000
+  return Math.round(number * 1000) / 1000;
 }
 
 function handleKeyboardInput(e) {
-  if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
-  if (e.key === '.') appendPoint()
-  if (e.key === '=' || e.key === 'Enter') evaluate()
-  if (e.key === 'Backspace') deleteNumber()
-  if (e.key === 'Escape') clear()
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === '.') appendPoint();
+  if (e.key === '=' || e.key === 'Enter') evaluate();
+  if (e.key === 'Backspace') deleteNumber();
+  if (e.key === 'Escape') clear();
   if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
-    setOperation(convertOperator(e.key))
+    setOperation(convertOperator(e.key));
 }
 
 function convertOperator(keyboardOperator) {
-  if (keyboardOperator === '/') return '÷'
-  if (keyboardOperator === '*') return '×'
-  if (keyboardOperator === '-') return '−'
-  if (keyboardOperator === '+') return '+'
+  if (keyboardOperator === '/') return '÷';
+  if (keyboardOperator === '*') return '×';
+  if (keyboardOperator === '-') return '−';
+  if (keyboardOperator === '+') return '+';
 }
 
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function substract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
 }
 
 function operate(operator, a, b) {
-  a = Number(a)
-  b = Number(b)
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case '+':
-      return add(a, b)
+      return add(a, b);
     case '−':
-      return substract(a, b)
+      return substract(a, b);
     case '×':
-      return multiply(a, b)
+      return multiply(a, b);
     case '÷':
-      if (b === 0) return null
-      else return divide(a, b)
+      if (b === 0) return null;
+      else return divide(a, b);
     default:
-      return null
+      return null;
   }
 }
