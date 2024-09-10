1db8874728698315fbfbe7a3a6b684ad31d8dc17
Tharun Raman <116884536+tharun977@users.noreply.github.com>
2024-09-10 20:01:55 +0530

    MusicPlayer

--- task3/script.js
+++ task3/script.js
@@ -12,7 +12,7 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = ['bandana', 'BeMine', 'KingShit'];
 
 // Keep track of song
 let songIndex = 2;
